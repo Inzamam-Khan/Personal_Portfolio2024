ca9a420f7f4289851e9c275a4163863f4ff428c3
Inzamam-khan <khanloveekhan>
2024-11-22 21:19:57 +0530

    animations added

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,7 +1,52 @@
 import userLogo from '../assets/bgImages/inzamamReactJs.jpeg'
 
 import {motion} from 'motion/react'
+
+
+
+import { useEffect, useState } from 'react'
+
+// import CursorBlinker from './Components/CursorBlinker'
+import { animate, useMotionValue, useTransform } from 'framer-motion';
+import CursorBlinker from '../Components/CursorBlinker';
 export const Hero=()=>{
+
+    const myName = "Inzamam Khan";
+    const myDesc=" Motivated and Creative Web developer,building applications with cutting-edge technologies.Proven success in designing,coding, and testing Web applications.A passion for discovering solutions and drive to always improve user Experience."
+    const count = useMotionValue(0);
+
+    const count1 = useMotionValue(0);
+    const rounded1 = useTransform(count1, (latest) => Math.round(latest));
+
+    const rounded = useTransform(count, (latest) => Math.round(latest));
+    const displayMyName = useTransform(rounded, (latest) =>
+      myName.slice(0, latest));
+    
+    const displayMyDesc= useTransform(rounded1, (latest) =>
+        myDesc.slice(0, latest));
+
+  
+    useEffect(() => {
+      const controls = animate(count, myName.length, {
+        type: "tween", // Not really needed because adding a duration will force "tween"
+        duration: 4,
+        ease: "easeInOut",
+        // delayChildren: 0.3,
+        // staggerChildren: 0.1
+      });
+      return controls.stop;
+    }, []);
+
+    useEffect(() => {
+        const controls = animate(count1, myDesc.length, {
+          type: "tween", // Not really needed because adding a duration will force "tween"
+          duration: 12,
+          ease: "easeInOut",
+          delay:4
+        });
+        return controls.stop;
+      }, []);
+
     return(
         
         <motion.div
@@ -13,16 +58,29 @@ export const Hero=()=>{
         className=" border-green-500 max-md:flex-col-reverse flex flex-1 flex-row items-center justify-center w-[100%] md:space-x-[100px] max-md:space-y-[25px] max-md:mb-20 md:mt-32 mb-3 max-md:mt-8 ">
 
                 {/* left main div */}
-            <div className=" flex flex-col  p-4 max-w-[40rem]">
-                <h3 id='hero' className="text-transparent bg-clip-text bg-gradient-to-br from-green-600 to-purple-800 border-blue-500 max-w-[20rem] text-7xl font-semibold ">
-                Inzamam Khan
+            <div className=" flex-col  p-4 w-[40rem]">
+
+                <motion.span id='hero' className=" text-transparent bg-clip-text bg-gradient-to-br from-green-600 to-purple-800 border-blue-500 max-w-[20rem] text-7xl font-semibold ">
                     
-                </h3>
+                {/* Inzamam Khan */}
+                    {displayMyName}
+
+                    
+                </motion.span>
+                <CursorBlinker/>
+              
+                
 
 
-                <p className=" mt-4 ">
-                Motivated and Creative Web developer,building applications with cutting-edge technologies.Proven success in designing,coding, and testing Web applications.A passion for discovering solutions and drive to always improve user Experience.
-                </p>
+                
+                
+
+
+
+             <motion.p className=" mt-4 ">
+                {/* Motivated and Creative Web developer,building applications with cutting-edge technologies.Proven success in designing,coding, and testing Web applications.A passion for discovering solutions and drive to always improve user Experience. */}
+                {displayMyDesc}
+                </motion.p> 
 
             </div>
 
