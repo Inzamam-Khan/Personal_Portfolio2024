685e4af8d90fdab25b29b5ba138636f6a137122f
Inzamam-khan <khanloveekhan>
2024-11-22 22:55:40 +0530

    minor issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,18 @@ import { Hero } from "./Sections/Hero"
 import { Projects } from "./Sections/Projects"
 import { Skills } from "./Sections/Skills"
 import { Toaster } from "react-hot-toast"
-
+import heroBg from '../src/assets/bgImages/heroBg.jpg'
 
 function App() {
   
 
   return (
-  <div className=" border-green-500  w-[100%] h-[100%] p-4  bg-hero bg-cover bg-center bg-fixed">
+  <div className="relative border-green-500  w-[100%] h-[100%] p-4  ">
+    <img src={heroBg} alt="" className="absolute top-0 left-0 h-[100%] w-screen z-0 " />
 
 <Toaster />
 
-      <div className="flex flex-col  border-blue-500 items-center  ">
+      <div className="flex flex-col  border-blue-500 items-center z-1 relative ">
         <Header/>
         <Hero/>
 
